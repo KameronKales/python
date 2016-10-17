922135a6dfb812b9d4f636e3e34ca26e9a068687
Kameron <Kaleskr@appstate.edu>
2016-10-17 08:39:04 -0400

    added example using variables & math.

---
+++ ex2.py
@@ -0,0 +1,10 @@
+# Set count_to equal to the sum of two big numbers
+
+addition = 72 + 23
+subtraction = 108-204
+multiplication = 108 * .5
+division = 108/9
+
+count_to = division * multiplication
+
+print count_to
