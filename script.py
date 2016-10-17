0493e8fa83afe0070d77d8f498492e6f4bb6fe41
Kameron <Kaleskr@appstate.edu>
2016-10-17 01:17:25 -0400

    added basic syntax file with some variables.

---
+++ script.py
@@ -0,0 +1,12 @@
+print "Welcome to Python!"
+my_variable = 10 
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+
+#### we will change the variable to 3, from 7.
+my_int = 7
+print my_int
+my_int = 3
+print my_int
